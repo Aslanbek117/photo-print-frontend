bcef684d680946859ff0149d0e8c5ba61e6a8b7d
Aslanbek Sekenov <a.sekenov117@gmail.com>
2021-01-16 17:26:05 +0600

    wip

--- src/components/steps/Steps.tsx
+++ src/components/steps/Steps.tsx
@@ -34,15 +34,24 @@ export const StepsComponent = (props: any) => {
             <List
                 itemLayout="horizontal"
                 dataSource={data}
-                renderItem={item => (
+                renderItem={(item, index) => (
                     <List.Item>
                         <List.Item.Meta
                             avatar={(
-                                <span>
-                                <img src={StepIcon} className="img" />
+                                <div className="icon">
+                                {/* <img src={StepIcon}  /> */}
+                                    <div className="icon-text">
+                                        {index + 1 }     
+                                    </div>
+                                </div>
+                                
+                            )}
+                            // description="Выберите страну и введите номер телефона. Нажмите «У меня нет мобильного телефона», если у вас его нет, и укажите резервную почту. "
+                            description={(
+                                <span className="primary-text">
+                                    Выберите страну и введите номер телефона. Нажмите «У меня нет мобильного телефона», если у вас его нет, и укажите резервную почту.
                                 </span>
                             )}
-                            description="Выберите страну и введите номер телефона. Нажмите «У меня нет мобильного телефона», если у вас его нет, и укажите резервную почту. "
                         />
                     </List.Item>
                 )}
--- src/components/tree-content/TreeContent.tsx
+++ src/components/tree-content/TreeContent.tsx
@@ -8,6 +8,7 @@ import { ArrowLeftOutlined } from '@ant-design/icons';
 import { Article } from '../article/Article';
 import { SearchModel } from '../../models/search/Search';
 import "./style.css";
+import ArrowLeft from './arrow-left.png';
 const { Content, Sider } = Layout;
 
 const { Text } = Typography;
@@ -26,15 +27,19 @@ export const TreeContent = (props: TreeContentProps) => {
 
     const [searchText, setSearchText] = useState('');
 
+    const [articleClicked, setArticleClicked] = useState(false);
+
     const onSelect = (selectedKeys: any, info: any) => {
         let index = selectedKeys.toString().lastIndexOf("/") + 1
         let navItem = selectedKeys.toString().slice(index, selectedKeys.toString().length);
         console.log("category", navItem);
         setSelectedNavItem(navItem)
+        setArticleClicked(false);
     };
 
     const onArticleClick = (title: string) => {
         console.log("article click", title);
+        setArticleClicked(true);
     }
 
     return (
@@ -43,45 +48,53 @@ export const TreeContent = (props: TreeContentProps) => {
 
                 <Sider theme='light' style={{ borderTopLeftRadius: '24px', paddingTop: '20px' }}>
                     <span style={{ fontFamily: 'Roboto', fontSize: "14px", lineHeight: '20px !important', letterSpacing: "-0.4px/" }}>
-                        <ArrowLeftOutlined style={{ paddingLeft: '5px', marginRight: '10px' }} />
-                        Вернуться на главную
-</span>
-                    {loading ? null : (
-                        <TreeView onSelect={onSelect} treeData={[]} token="" items={props.treeData} />
-                    )}
+                        {/* <ArrowLeftOutlined style={{ paddingLeft: '5px', marginRight: '10px' }} /> */}
+                        <img src={ArrowLeft} style={{ paddingLeft: '5px', width: 18, height: 12 }} />
+                        <span style={{ paddingLeft: '5px' }}>
+                            Вернуться на главную
+                        </span>
+                    </span>
+                    <div style={{paddingTop: '15px'}}>
+                        {loading ? null : (
+                            <TreeView onSelect={onSelect} treeData={[]} token="" items={props.treeData} />
+                        )}
+                    </div>
                 </Sider>
 
                 <Divider type="vertical" style={{ height: "100%", margin: "0 0px" }} />
-
                 <Content style={{ padding: '0 24px', minHeight: 280, backgroundColor: 'white', paddingTop: '15px', borderTopRightRadius: '24px' }} >
-                    <Typography>
-                        {props.items.length == 0 ? (
-                            <span className="title">
-                                В Базе знаний нет статьи по запросу "{searchText}"
+
+
+
+                    {articleClicked ? (
+                        <Article />
+                    ) : (
+                            <>
+                                <Typography>
+                                    {props.items.length == 0 ? (
+                                        <span className="title">
+                                            В Базе знаний нет статьи по запросу "{searchText}"
                             </span>
-                        ) : ''}
+                                    ) : ''}
 
-                        <Title> {selectedNavItem} </Title>
-                        {(props.items.length == 1) ? (
+                                    <Title> {selectedNavItem} </Title>
+                                    {(props.items.length == 1) ? (
 
-                            <Text strong={true} style={{ backgroundColor: "rgb(249,250,250)", color: "black !important" }}>
-                                найдена: 1 статья
+                                        <Text strong={true} style={{ backgroundColor: "rgb(249,250,250)", color: "black !important" }}>
+                                            найдена: 1 статья
                             </Text>
-                        ) : (
+                                    ) : (
 
-                                <Text strong={true} style={{ backgroundColor: "rgb(249,250,250)", color: "black !important" }}>
-                                    найдено: {props.items.length} статьи
+                                            <Text strong={true} style={{ backgroundColor: "rgb(249,250,250)", color: "black !important" }}>
+                                                найдено: {props.items.length} статьи
                                 </Text>
-                            )}
+                                        )}
+                                </Typography>
+                                <Divider type="horizontal" />
 
-                    </Typography>
-
-                    <Divider type="horizontal" />
-
-                    <ItemList items={props.items}  onClick={onArticleClick} />
-
-
-
+                                <ItemList items={props.items} onClick={onArticleClick} />
+                            </>
+                        )}
 
                 </Content>
             </Layout>
