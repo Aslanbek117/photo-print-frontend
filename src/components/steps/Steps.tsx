4b32c668057b9af50b3b674994891da3b7a0faed
Aslanbek Sekenov <a.sekenov117@gmail.com>
2021-01-20 10:50:19 +0600

    fix conditions and process

--- src/components/steps/Steps.tsx
+++ src/components/steps/Steps.tsx
@@ -1,59 +1,95 @@
 import React, { useState } from 'react';
-import { Tabs, Steps, List } from 'antd';
+import { Tabs, Steps, List, Image } from 'antd';
 import ClientIcon from './client-icon.png';
 import OperatorIcon from './operator-icon.png'
 import StepIcon from './step-icon.png';
 import './steps.css'
+import { ProcessModel } from '../../models/search/Search';
 const { TabPane } = Tabs;
 const { Step } = Steps;
 
 
-interface ConditionProps {
-    isShow: boolean;
+interface StepsProps {
+    data: ProcessModel[];
 }
 
 const data = [
     {
-      title: 'Ant Design Title 1',
+        title: 'Ant Design Title 1',
     },
     {
-      title: 'Ant Design Title 2',
+        title: 'Ant Design Title 2',
     },
     {
-      title: 'Ant Design Title 3',
+        title: 'Ant Design Title 3',
     },
     {
-      title: 'Ant Design Title 4',
+        title: 'Ant Design Title 4',
     },
-  ];
+];
 
-export const StepsComponent = (props: any) => {
+export const StepsComponent = (props: StepsProps) => {
 
     return (
         <>
             <List
                 itemLayout="horizontal"
-                dataSource={data}
+                dataSource={props.data}
                 renderItem={(item, index) => (
-                    <List.Item>
-                        <List.Item.Meta
-                            avatar={(
-                                <div className="icon">
-                                {/* <img src={StepIcon}  /> */}
-                                    <div className="icon-text">
-                                        {index + 1 }     
+                    <>
+                        <List.Item>
+                            <List.Item.Meta
+                                avatar={(
+                                    <div className="icon">
+                                        {/* <img src={StepIcon}  /> */}
+                                        <div className="icon-text">
+                                            {index + 1}
+                                        </div>
                                     </div>
-                                </div>
-                                
-                            )}
-                            // description="Выберите страну и введите номер телефона. Нажмите «У меня нет мобильного телефона», если у вас его нет, и укажите резервную почту. "
-                            description={(
-                                <span className="primary-text">
-                                    Выберите страну и введите номер телефона. Нажмите «У меня нет мобильного телефона», если у вас его нет, и укажите резервную почту.
-                                </span>
-                            )}
-                        />
-                    </List.Item>
+
+                                )}
+                                // description="Выберите страну и введите номер телефона. Нажмите «У меня нет мобильного телефона», если у вас его нет, и укажите резервную почту. "
+                                description={(
+                                    <>
+                                        <span className="primary-text">
+                                            {item.description}
+                                            {item.files.map(f => (
+
+                                                <>
+                                                    <div>
+
+                                                        <div className="img-border">
+                                                            <Image
+                                                                width={36}
+                                                                className="img-style"
+                                                                src={"http://halyk-wiki.cfp.corp.p-s.kz/file-server/" + f.full_directory}
+                                                            />
+                                                            <span className="img-title"> {f.name} </span>
+                                                        </div>
+
+                                                    </div>
+                                                </>
+                                            ))}
+                                        </span>
+
+                                        <div>
+
+                                            {item.files.map(f => {
+                                                <>
+                                                    <img src={"http://halyk-wiki.cfp.corp.p-s.kz/file-server/Kredity/Novyj%20kredit/Podkategorija%201/new-credit.png"} >
+                                                        xui
+                                                </img>
+                                                    <span> {f.full_directory} </span>
+                                                </>
+                                            })}
+                                        </div>
+                                    </>
+                                )}
+                            />
+                        </List.Item>
+                    </>
+
+
                 )}
             />
         </>
