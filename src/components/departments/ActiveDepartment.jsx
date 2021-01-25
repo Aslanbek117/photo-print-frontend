2d5ea79bf6f27f05a6a9e8ff5c06b44a2519b835
Makhabbat <m.galym@p-s.kz>
2021-01-25 16:18:59 +0600

    wip: added ActiveDepartment

---
+++ src/components/departments/ActiveDepartment.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+
+import CircleSmallLogo from './circleSmallLogo.svg'
+import './department.css';
+
+const daysOfWeek = [
+    { title: 'Пн' }, 
+    { title: 'Вт' }, 
+    { title: 'Ср' }, 
+    { title: 'Чт' },
+    { title: 'Пт' },
+    { title: 'Сб' },
+    { title: 'Вс' }
+    ]
+
+const ActiveDepartment = (department: any) => {
+    
+    return (
+        <div className="active-department">
+            <div className="active-department-header">
+                <img alt="" src={CircleSmallLogo}/>
+                <div className="active-department-header-right">
+                    <p style={{fontSize: 15, margin: 0, fontWeight: 'bold'}}>Отделение №1</p>
+                    <p style={{fontSize: 15, margin: 0}}>г. Алматы, проспект Абая, 21</p>
+                </div>
+            </div>
+            <div className="active-department-body">
+                {daysOfWeek.map((day, i) => {
+                    const active = day?.title === 'Чт'
+                    return (
+                    <div className={`schedule-item ${active && "active-schedule-item"}`}>
+                        <p className="week-day">{day?.title}</p>
+                        <p className="work-times">08:00</p>
+                        <p className="work-times">-</p>
+                        <p className="work-times">22:00</p>
+                    </div>
+                )})}
+            </div>
+        </div>
+    )
+}
+
+export default ActiveDepartment;
--- src/components/modals/DepartmentModal.tsx
+++ src/components/modals/DepartmentModal.tsx
@@ -1,38 +1,45 @@
-import React, { useState, useEffect } from 'react';
-import { Modal } from 'antd';
-import YandexMap  from '../departments/Department';
-import SearchIcon from './search-icon-2.png';
-import './style.css';
+import React, { useState, useEffect } from "react";
+import { Modal } from "antd";
+import YandexMap from "../departments/Department";
+import SearchIcon from "./search-icon-2.png";
+import CloseIcon from "./close.svg";
+import "./style.css";
 
 interface ModalProps {
-    isVisible: boolean;
-    onOk: () => void;
-    onCancel: () => void;
-    title: string;
+  isVisible: boolean;
+  onOk: () => void;
+  onCancel: () => void;
+  title: string;
 }
 
 export const DepartmentModal = (props: ModalProps) => {
-    const [searchValue, setSearchValue] = useState<string>('');
+  const [searchValue, setSearchValue] = useState<string>("");
 
-
-
-    return (
-        <>
-            <Modal title={props.title} visible={props.isVisible} onOk={props.onOk} onCancel={props.onCancel} footer={null} style={{minHeight: 620}}>
-                <div className="search-input-modal" >
-                    <i className="search-icon-modal">
-                        <img src={SearchIcon} />
-                    </i>
-                    <input className="search-input-field-modal" placeholder="Введите адрес" value={searchValue} onChange={(event) => {
-                        setSearchValue(event.target.value);
-                    }}
-                    />
-                </div>
-                <div style={{marginTop: 48, height: '100%'}}>
-                    <YandexMap />
-                </div>
-
-            </Modal>
-        </>
-    )
-}
+  return (
+    <Modal
+      title={<p className="department-modal-title">{props.title}</p>}
+      visible={props.isVisible}
+      onOk={props.onOk}
+      onCancel={props.onCancel}
+      footer={null}
+      style={{ minHeight: 620 }}
+      closeIcon={<img src={CloseIcon} />}>
+      <div className="search-input-modal">
+        <i className="search-icon-modal">
+          <img src={SearchIcon} />
+        </i>
+        <input
+          className="search-input-field-modal"
+          placeholder="Введите адрес"
+          value={searchValue}
+          onChange={(event) => {
+            setSearchValue(event.target.value);
+          }}
+        />
+      </div>
+      <div style={{ marginTop: 15, height: "100%" }}>
+        <YandexMap />
+      </div>
+    </Modal>
+  );
+};
