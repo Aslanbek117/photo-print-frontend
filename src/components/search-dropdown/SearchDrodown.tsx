a61d6d44e94c8e9c20308cd55506ff4a11d5e40a
Makhabbat <m.galym@p-s.kz>
2021-01-26 16:46:37 +0600

    refactor: search

--- src/components/search-dropdown/SearchDrodown.tsx
+++ src/components/search-dropdown/SearchDrodown.tsx
@@ -1,43 +1,36 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './search-dropdown.css';
-import ArrowIcon from './arrow-icon.png'
-import { SearchModel, Article } from '../../models/search/Search';
-import { getByDisplayValue } from '@testing-library/react';
-
+import Icon from '../icon';
+import { Article } from '../../models/search/Search';
 
 interface SearchDropdownProps {
-    items: Article[];
-    onClick: (title: string) => void;
+  items: Article[];
+  onClick: (title: string) => void;
 }
 
 export const SearchDropdown = (props: SearchDropdownProps) => {
+  const getValue = (title: string) => {
+    console.log('LI VALUE', title);
+  };
 
-    const getValue = (title: string) => {
-        console.log("LI VALUE", title);
-    }
+  const listItems = props.items.map((item) => (
+    <li value={item.title} onClick={() => props.onClick(item.title)}>
+      <div className="search-result-item">
+        <Icon icon="corner-down-right" width={20} height={18} />
+        <span style={{ paddingLeft: '20px', fontWeight: 500, fontSize: 16, color: '#171717' }}>
+          {item.title}
+        </span>
+      </div>
+    </li>
+  ));
 
-    const listItems = props.items.map((item) =>
-        <li value={item.title} onClick={() => props.onClick(item.title)}>
-            <i>
-                <img src={ArrowIcon} />
-            </i>
-            <span style={{ paddingLeft: '20px', fontWeight: 500, fontSize: 16, color: '#171717' }}>
-                {item.title}
-            </span>
-        </li>
-    );
-
-    return (
-        <>
-            <div style={{ paddingTop: props.items.length > 0 ? '80px' : '0px', width: '100%' }}>
-                <div className="on-hover" style={{ paddingLeft: '35px', paddingBottom: '15px' }}>
-                    <ul style={{ listStyleType: 'none', margin: 0, padding: 0 }} >
-                        {listItems}
-                    </ul>
-                </div>
-            </div>
-
-        </>
-    )
-}
-
+  return (
+    <>
+      <div style={{ paddingTop: props.items.length > 0 ? '80px' : '0px', width: '100%' }}>
+        <div style={{ paddingBottom: '15px' }}>
+          <ul style={{ listStyleType: 'none', margin: 0, padding: 0 }}>{listItems}</ul>
+        </div>
+      </div>
+    </>
+  );
+};
