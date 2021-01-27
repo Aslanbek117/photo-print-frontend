3b85fff812528e575f442b7d84f5ce26328bebc9
Makhabbat <m.galym@p-s.kz>
2021-01-27 18:08:21 +0600

    refactor: updated StepsComponent

--- src/components/steps/Steps.tsx
+++ src/components/steps/Steps.tsx
@@ -1,72 +1,63 @@
-import React, { useState } from 'react';
-import { Tabs, Steps, List, Image } from 'antd';
-import ClientIcon from './client-icon.png';
-import OperatorIcon from './operator-icon.png'
-import StepIcon from './step-icon.png';
-import './steps.css'
+import React from 'react';
+import { List, Image } from 'antd';
+import Text from '../text';
+import './steps.css';
 import { ProcessModel } from '../../models/search/Search';
-const { TabPane } = Tabs;
-const { Step } = Steps;
-
 
 interface StepsProps {
-    data: ProcessModel[];
+  data: ProcessModel[];
 }
 
 export const StepsComponent = (props: StepsProps) => {
-
-    return (
-        <>
-            <List
-                itemLayout="horizontal"
-                dataSource={props.data}
-                renderItem={(item, index) => (
-                    <>
-                        <List.Item>
-                            <List.Item.Meta
-                                avatar={(
-                                    <div className="icon">
-                                        {/* <img src={StepIcon}  /> */}
-                                        <div className="icon-text">
-                                            {index + 1}
-                                        </div>
-                                    </div>
-
-                                )}
-                                // description="Выберите страну и введите номер телефона. Нажмите «У меня нет мобильного телефона», если у вас его нет, и укажите резервную почту. "
-                                description={(
-                                    <>
-                                        <span className="primary-text">
-                                            {item.description}
-
-                                        </span>
-                                        <br />
-                                        {item.files.map(f => (
-
-                                            <>
-                                                <div style={{ display: 'inline-block' }}>
-
-                                                    <div className="img-border">
-                                                        <Image
-                                                            width={36}
-                                                            className="img-style"
-                                                            src={"http://halyk-wiki.cfp.corp.p-s.kz/file-server/" + f.name}
-                                                        />
-                                                        <span className="img-title"> {f.name} </span>
-                                                    </div>
-
-                                                </div>
-                                            </>
-                                        ))}
-                                    </>
-                                )}
+  return (
+    <>
+      <List
+        itemLayout="horizontal"
+        dataSource={props.data}
+        renderItem={(item, index) => (
+          <>
+            <List.Item>
+              <List.Item.Meta
+                avatar={
+                  <div className="index-container">
+                    <Text type="subtitle2" color="#008060">
+                      {index + 1}
+                    </Text>
+                  </div>
+                }
+                // description="Выберите страну и введите номер телефона. Нажмите «У меня нет мобильного телефона», если у вас его нет, и укажите резервную почту. "
+                description={
+                  <>
+                    <Text>{item.description}</Text>
+                    <br />
+                    {item.files.map((f) => (
+                      <>
+                        <div
+                          style={{
+                            display: 'flex',
+                            flexWrap: 'wrap',
+                            justifyContent: 'space-between',
+                          }}
+                        >
+                          <div className="img-border">
+                            <Image
+                              className="img-style"
+                              src={'http://halyk-wiki.cfp.corp.p-s.kz/file-server/' + f.name}
                             />
-                        </List.Item>
-                    </>
-                )}
-            />
-        </>
-
-    )
-}
-
+                            <Text type="small" color="#171717" style={{ maxWidth: 120 }}>
+                              {f.name}
+                            </Text>
+                          </div>
+                        </div>
+                      </>
+                    ))}
+                  </>
+                }
+              />
+            </List.Item>
+          </>
+        )}
+      />
+    </>
+  );
+};
