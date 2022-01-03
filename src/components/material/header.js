9a479439c4beaca0adfec0aa8324c970150426b0
asekenov <a.sekenov117@gmail.com>
2022-01-03 22:30:27 +0600

    remove navbar image && add shop icon

--- src/components/material/header.js
+++ src/components/material/header.js
@@ -2,8 +2,6 @@ import * as React from "react";
 import { Collapse } from "bootstrap";
 import logo from "./logo2.png";
 
-
-
 export default function SiteHeader() {
   const [visible, setVisible] = React.useState(false);
 
@@ -13,67 +11,78 @@ export default function SiteHeader() {
     setWidth(window.innerWidth);
   }
 
-  
-  React.useEffect(() => {
-   
-  }, []);
-
+  React.useEffect(() => {}, []);
 
   return (
     <>
-        
-        <nav class="navbar sticky-top navbar-expand-md navbar-light navbar-left" id="navbar" style={{borderBottom: '2px solid #F88C00', backgroundColor: 'white'}} >
-          <div class="container py-0 py-lg-0 px-lg-0">
-            <a class="navbar-brand" href="/">
-              <img
-                class=" d-md-inline-block"
-                src={logo}
-                alt="Universal logo"
-              />
-            </a>
-            <button
-              class="navbar-toggler text-primary border-primary order-first"
-              type="button"
-              data-bs-toggle="collapse"
-              data-bs-target="#navigationCollapse"
-              aria-controls="navigationCollapse"
-              aria-expanded="false"
-              aria-label="Toggle navigation"
-              // onClick={() => setVisible(!visible)}
-              style={{backgroundColor: 'white'}}
-            >
-              <span class="sr-only">Окрыть меню</span>
-              <i class="fas fa-align-justify"></i>
-            </button>
-            <div class="collapse navbar-collapse" id="navigationCollapse">
-              <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-                <li class="nav-item dropdown">
-                  <a
-                    class="nav-link"
-                    href="/"
-                    role="button"
-                    aria-expanded="false"
-                    
-                  >
-                    Модульные картины
-                  </a>
-                </li>
+      <nav
+        class="navbar sticky-top navbar-expand-md navbar-light navbar-left"
+        id="navbar"
+        style={{ borderBottom: "2px solid #F88C00", backgroundColor: "white" }}
+      >
+        <div class="container py-0 py-lg-0 px-lg-0">
+          {/* <a class="navbar-brand" href="/">
+            <img class=" d-md-inline-block" src={logo} alt="Universal logo" />
+          </a> */}
 
-                <li class="nav-item dropdown">
-                  <a
-                    class="nav-link"
-                    href="/"
-                    role="button"
-                    aria-expanded="false"
-                    
-                  >
-                    Фотообои
-                  </a>
-                </li>
-              </ul>
-            </div>
+          <a class="d-md-none  navbar-brand" >
+            <i class="fas fa-shopping-cart" > пусто</i>
+          </a>
+
+          <button
+            class="navbar-toggler text-primary border-primary order-first"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navigationCollapse"
+            aria-controls="navigationCollapse"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+            // onClick={() => setVisible(!visible)}
+            style={{ backgroundColor: "white" }}
+          >
+            <span class="sr-only">Окрыть меню</span>
+            <i class="fas fa-align-justify"></i>
+          </button>
+
+          <div class="collapse navbar-collapse" id="navigationCollapse">
+            <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+              <li class="nav-item dropdown">
+                <a
+                  class="nav-link"
+                  href="/"
+                  role="button"
+                  aria-expanded="false"
+                >
+                  Модульные картины
+                </a>
+              </li>
+
+
+              <li class="nav-item dropdown">
+                <a
+                  class="nav-link"
+                  href="/"
+                  role="button"
+                  aria-expanded="false"
+                >
+                  Фотообои
+                </a>
+              </li>
+
+              <li class="d-none d-sm-block nav-item">
+                <a
+                  class="nav-link"
+                  href="/"
+                  role="button"
+                  aria-expanded="false"
+                >
+                  <i class="fas fa-shopping-cart" > пусто</i>
+                </a>
+              </li>
+            </ul>
           </div>
-        </nav>
+        </div>
+      </nav>
     </>
   );
 }
