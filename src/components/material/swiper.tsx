8cdcc822d8d4c2af20a73ddebbc033e07cbf9da4
asekenov <a.sekenov117@gmail.com>
2022-01-03 19:52:08 +0600

    swiper done

--- src/components/material/swiper.tsx
+++ src/components/material/swiper.tsx
@@ -5,6 +5,13 @@ import { Navigation } from "swiper";
 // Direct React component imports
 import { Swiper, SwiperSlide } from "swiper/react/swiper-react.js";
 
+import first from "./2x2.png";
+import second from "./3x3.png";
+import one from "./1x1.png";
+import not from "./2x2_not.png";
+import not3x3 from "./3x3_not.png";
+import not1x1 from "./1x1_not.png";
+import is from "./out_5.png";
 import SwiperCore, { Grid } from "swiper";
 // Styles must use direct files imports
 // Styles must use direct files imports
@@ -17,23 +24,20 @@ import "./app.css";
 import { read } from "fs";
 import { relative } from "path/posix";
 
-
 SwiperCore.use([Grid, Navigation]);
 
 interface Props {
-    onClick: Function
-    onSlideChange: Function
-    slideTo: number
+  onClick: Function;
+  onSlideChange: Function;
+  slideTo: number;
 }
 
-export const CustomSwiper = (props: Props)  => {
+export const CustomSwiper = (props: Props) => {
   const [swiperInstance, setSwiperInstance] = React.useState<SwiperCore>();
 
   const [value, setValue] = React.useState(0);
 
   const [index, setIndex] = React.useState(0);
-
-
 
   const handleExternalChangeSlide = (newSlideIndexToShow) => {
     // console.log("NEW", newSlideIndexToShow.realIndex)
@@ -43,62 +47,66 @@ export const CustomSwiper = (props: Props)  => {
     // let currentSlide = swiperInstance?.slides[newSlideIndexToShow.realIndex]
     // swiperInstance?.slideTo(newSlideIndexToShow.realIndex);
     setValue(newSlideIndexToShow.realIndex);
- }
+  };
 
- const nextTo= () => {
-   swiperInstance?.slideNext();
- }
- 
+  const nextTo = () => {
+    swiperInstance?.slideNext();
+  };
+
   return (
-    <div className="swiper">
-      <Swiper
-        spaceBetween={30}
-        grid={{"rows": 2}}
-        slidesPerView={3}
-        slidesPerGroup={6}
-        navigation={true}
-        className="mySwiper"
-        initialSlide={props.slideTo}
-       
-        onTransitionEnd={(swiper) => {
-          props.onSlideChange(swiper.realIndex*2)
-        }}
-        onSlideChange={(swiper) => 
-          
-        //   (
-          
-        //   setValue(swiper.realIndex* 2),
-        //   console.log(swiper.realIndex*2),
-        //   console.log("SLIDE_TO", props.slideTo),
-        //   // props.onSlideChange(3),
-        //   swiper.slideTo(6)
-        // )
-      {
-        // if (swiper.realIndex == 0) {
-        //   if (swiper.ri)
-        // }
-      }
-      
-      }
-
-      >
-        <SwiperSlide onClick={ () => props.onClick(1)}  >Slide 1</SwiperSlide>
-        <SwiperSlide onClick={ () => props.onClick(2)}>Slide 2</SwiperSlide>
-        <SwiperSlide onClick={ () => props.onClick(3)}>Slide 3</SwiperSlide>
-        <SwiperSlide onClick={ () => props.onClick(4)}>Slide 4</SwiperSlide>
-        <SwiperSlide onClick={ () => props.onClick(5)}>Slide 5</SwiperSlide>
-        <SwiperSlide onClick={ () => props.onClick(6)}>Slide 6</SwiperSlide>
-        <SwiperSlide onClick={ () => props.onClick(7)}>Slide 7</SwiperSlide>
-        <SwiperSlide onClick={ () => props.onClick(8)}>Slide 8</SwiperSlide>
-        <SwiperSlide onClick={ () => props.onClick(9)}>Slide 9</SwiperSlide>
-        <SwiperSlide onClick={ () => props.onClick(10)}>Slide 10</SwiperSlide>
-        <SwiperSlide onClick={ () => props.onClick(11)}>Slide 11</SwiperSlide>
-        <SwiperSlide onClick={ () => props.onClick(12)}>Slide 12</SwiperSlide>
-      </Swiper>
-      {/* <button onClick={nextTo} type='button'>next</button> */}
-    </div>
+    <>
+      <div className="swiper">
+        <Swiper
+          spaceBetween={30}
+          grid={{ rows: 2 }}
+          slidesPerView={3}
+          slidesPerGroup={6}
+          navigation={true}
+          className="mySwiper"
+          initialSlide={props.slideTo}
+          onTransitionEnd={(swiper) => {
+            props.onSlideChange(swiper.realIndex * 2);
+          }}
+          style={{ border: "1px solid black" }}
+        >
+          <SwiperSlide onClick={() => props.onClick(1)}>
+            <span className="sepia">
+              <img src={first} onClick={() => props.onClick(1)} />
+            </span>
+          </SwiperSlide>
+          <SwiperSlide onClick={() => props.onClick(2)}>
+            <span className="sepia">
+              <img src={second} onClick={() => props.onClick(2)} />
+            </span>
+          </SwiperSlide>
+          <SwiperSlide onClick={() => props.onClick(3)}>
+            <span className="sepia">
+              <img src={one} onClick={() => props.onClick(3)} />
+            </span>
+          </SwiperSlide>
+          <SwiperSlide onClick={() => props.onClick(4)}>
+            <span className="sepia">
+              <img src={not} onClick={() => props.onClick(4)} />
+            </span>
+          </SwiperSlide>
+          <SwiperSlide onClick={() => props.onClick(5)}>
+            <span className="sepia">
+              <img src={not3x3} onClick={() => props.onClick(5)} />
+            </span>
+          </SwiperSlide>
+          <SwiperSlide onClick={() => props.onClick(6)}>
+            <span className="sepia">
+              <img src={not1x1} onClick={() => props.onClick(6)} />
+            </span>
+          </SwiperSlide>
+          <SwiperSlide onClick={() => props.onClick(7)}>Slide 7</SwiperSlide>
+          <SwiperSlide onClick={() => props.onClick(8)}>Slide 8</SwiperSlide>
+          <SwiperSlide onClick={() => props.onClick(9)}>Slide 9</SwiperSlide>
+          <SwiperSlide onClick={() => props.onClick(10)}>Slide 10</SwiperSlide>
+          <SwiperSlide onClick={() => props.onClick(11)}>Slide 11</SwiperSlide>
+          <SwiperSlide onClick={() => props.onClick(12)}>Slide 12</SwiperSlide>
+        </Swiper>
+      </div>
+    </>
   );
 };
-
-
-
