d078c5c7353e957750c2daa41ce261870cb6dac2
asekenov <a.sekenov117@gmail.com>
2022-01-05 03:17:31 +0600

    add index parsin

--- server.js
+++ server.js
@@ -3,6 +3,13 @@ const app = express();
 let listenPort = process.env.PORT || 3000;
 
 app.use(express.static("build"))
+
+ // Express serve up index.html file if it doesn't recognize route
+ const path = require('path');
+ app.get('*', (req, res) => {
+   res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+ });
+ 
 app.listen(listenPort, ()=> {
   console.log("server running on port "+ listenPort);
 })
