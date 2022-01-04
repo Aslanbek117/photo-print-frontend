30dabe5d0b5834bb3b101920c48ff388d60d3960
asekenov <a.sekenov117@gmail.com>
2022-01-05 03:32:50 +0600

    deploye

--- server.js
+++ server.js
@@ -2,14 +2,19 @@ const express = require("express");
 const app = express();
 let listenPort = process.env.PORT || 3000;
 
-app.use(express.static("build"))
+// app.use(express.static("build"))
 
  // Express serve up index.html file if it doesn't recognize route
- const path = require('path');
- app.get('*', (req, res) => {
-   res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
- });
- 
+//  const path = require('path');
+//  app.get('*', (req, res) => {
+  //  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+//  });
+
+server.configure(function(){
+  server.use('/media', express.static(__dirname + '/media'));
+  server.use(express.static(__dirname + '/public'));
+});
+
 app.listen(listenPort, ()=> {
   console.log("server running on port "+ listenPort);
 })
