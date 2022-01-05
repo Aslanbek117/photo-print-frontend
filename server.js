6c838794cf75d12405decf95dd79747ef625100e
asekenov <a.sekenov117@gmail.com>
2022-01-05 16:30:28 +0600

    test

--- server.js
+++ server.js
@@ -1,22 +1,23 @@
 const express = require("express");
 const path = require('path');
 const app = express();
+var favicon = require('serve-favicon');
 let listenPort = process.env.PORT || 3000;
 
 
  //здесь наше приложение отдаёт статику
  app.use(express.static(__dirname));
  app.use(express.static(path.join(__dirname, 'build')));
- 
+ app.use(favicon(__dirname + '/public/favicon.ico'));
  //простой тест сервера
  app.get('/ping', function (req, res) {
   return res.send('pong');
  });
  
+
  //обслуживание html
  app.get('/*', function (req, res) {
  res.sendFile(path.join(__dirname, 'build', 'index.html'));
  });
  app.listen(listenPort);
 
- 
